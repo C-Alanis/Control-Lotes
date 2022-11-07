4fe956807a8289f2311cdd82154bd98d959fe1b2
Cesar Alanis <cesar.alanis@na.denso.com>
2022-11-07 14:23:40 -0600

    Se agregaron los estilos

--- SAT.php
+++ SAT.php
@@ -6,7 +6,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Datos AT</title>
 </head>
-<body>
-    
+
+<body style="background-color: lightsteelblue;">
+    <nav class="navbar bg-white border-bottom border-secondary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php">
+                <img src="public/css/images/SECURITY.jpg" alt="Logo"
+                    style="width: 210px; height: 60px; margin-top: -10px; margin-bottom: -10px;" class="d-inline-block align-text-top">
+            </a>
+        </div>
+    </nav>
+    <main class="container contenedor">
+        <div class="py-5 container text-center">
+            
+        </div>
+
+    </main>
 </body>
+
 </html>
