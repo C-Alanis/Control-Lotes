48244d1e3ab40ab952ec270ae9e2fc3bc570eb50
Cesar Alanis <cesar.alanis@na.denso.com>
2022-11-07 14:09:01 -0600

    Prueba dos

---
+++ config/SAT.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registro AT</title>
+</head>
+<body>
+    
+<body style="background-color: lightsteelblue;">
+    <nav class="navbar bg-white border-bottom border-secondary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php">
+                <img src="public/css/images/SECURITY.jpg" alt="Logo"
+                    style="width: 210px; height: 60px; margin-top: -10px; margin-bottom: -10px;" class="d-inline-block align-text-top">
+            </a>
+        </div>
+    </nav>
+    <main class="container contenedor">
+        <div class="py-5 container text-center">
+            <div class="row g-2">
+                <div class="col-6">
+                    <div class="p-5 border-end border-bottom border-secondary bg-light-white"><a
+                            class="p-5 btn btn-lg col-md-5 fs-1 text-light" style="background-color:#242a30;" href="vct.php">V C T</a></div>
+                </div>
+                <div class="col-6">
+                    <div class="p-5 border-bottom border-start border-secondary bg-light-white"><a
+                            class="p-5 btn btn-lg col-md-5 fs-1 text-light" style="background-color:#242a30;" href="ocv.php">O C V</a></div>
+                </div>
+                <div class="col-6">
+                    <div class="p-5 border-top border-end border-secondary bg-light-white"><a
+                            class="p-5 btn btn-lg col-md-5 fs-1 text-light" style="background-color:#242a30;" href="vb.php">V B</a></div>
+                </div>
+                <div class="col-6">
+                    <div class="p-5 border-top border-start border-secondary bg-light-white"><a
+                            class="p-5 btn btn-lg col-md-5 fs-1 text-light" style="background-color:#242a30;" href="at.php">A T</a></div>
+                </div>
+            </div>
+        </div>
+
+    </main>
+</body>
+
+</body>
+</html>
