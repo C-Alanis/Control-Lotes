fb183fb9fdf424f95121fa4a41a86f6f639ceb9a
Cesar Alanis <cesar.alanis@na.denso.com>
2022-11-08 09:25:40 -0600

    Config fecha y hora

--- SAT.php
+++ SAT.php
@@ -11,50 +11,69 @@
     <script src="style/js/bootstrap.bundle.min.js"></script>
 </head>
 
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"
+    integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
 
-    <nav class="navbar navbar-expand-lg bg-light">
+<body>
+
+    <nav class="navbar navbar-expand-lg bg-danger">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
+            <a class="navbar-brand text-white fs-2 fw-bold" href="#">CONTROL DE LOTES</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                 aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">CAMBIO CAMBIO</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                            aria-expanded="false">
-                            Dropdown
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li>
-                                <hr class="dropdown-divider">
-                            </li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link disabled">Disabled</a>
-                    </li>
-                </ul>
-                <form class="d-flex" role="search">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
-            </div>
         </div>
+
+        <form class="container offset-md-7 text-center text-white" role="search">
+            <div class="row gx-5">
+            <div class="col h4">
+                <h4>Fecha:&nbsp;</h4>
+                <script>
+                date = new Date().toLocaleDateString();
+                document.write(date);
+                </script>
+            </div>
+
+            <div class="col">
+            <h4>Hora:&nbsp;</h4>
+            <h4 id="HoraActual">   </h4>
+            </div>
+
+            </div>
+
+        </form>
     </nav>
 
-<body>
+
+
+    
+
+
+
+
 </body>
+
+
+<script>
+// JavaScript code
+showTime();
+
+function showTime() {
+    myDate = new Date();
+    hours = myDate.getHours();
+    minutes = myDate.getMinutes();
+    seconds = myDate.getSeconds();
+    if (hours < 10) hours = 0 + hours;
+
+    if (minutes < 10) minutes = "0" + minutes;
+
+    if (seconds < 10) seconds = "0" + seconds;
+
+    $("#HoraActual").text(hours + ":" + minutes + ":" + seconds);
+    setTimeout("showTime()", 1000);
+}
+</script>
 
 </html>
