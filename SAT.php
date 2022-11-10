17ba72c13034a6d289b067e4176382383151cd45
Cesar Alanis <cesar.alanis@na.denso.com>
2022-11-10 10:02:03 -0600

    Limite de consultas a uno

--- SAT.php
+++ SAT.php
@@ -11,7 +11,7 @@ $page=$url;
 $sec="10";
 
 include("config/conexion.php");
-$m_actual="SELECT * FROM m_actual";
+$modelos="SELECT modelo FROM modelos ORDER BY id DESC LIMIT 1";
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -58,47 +58,25 @@ $m_actual="SELECT * FROM m_actual";
             </div>
         </form>
     </nav>
-  
+
     <main class="m-5 p-5">
-    
+
         <div class="border border-dark col-md-3 text-center">
-  
+
             <div class="py-3">
                 <div class="py-3">
-                    
+
                     <h1 class="">Modelo Actual</h1>
-<<<<<<< HEAD
                     <?php
-    $resultado=mysqli_query($conexion,$m_actual);
+    $resultado=mysqli_query($conexion,$modelos);
 
     while($row=mysqli_fetch_assoc($resultado)){?>
-    <h1 class="col"><?php echo $row["T_Modelo"];?></h1><?php }?>
-                    
-=======
-                    <h1 class=""></h1>
->>>>>>> c3341b15ebf8bfa6c704504a82e10c79f09c499e
+                    <h1 class="col"><?php echo $row["modelo"];?></h1><?php }?>
+
                 </div>
-           
+
             </div>
         </div>
-
-    <!--- centrar en canvas --->
-       <!--<style>
-    .container-canvas {
-        /* This could be done in one single declaration. See the extended sample. */
-        margin-right: auto;
-        margin-left: auto;
-        width: 800px;
-    }
-</style>
-
-<div class="container-canvas">
-    <canvas width="800" height="480"></canvas>
-</div>
--->                
-            
-
-       
 
         <div class="my-5 border border-dark col-3 text-center">
             <div class="py-3">
@@ -110,12 +88,12 @@ $m_actual="SELECT * FROM m_actual";
                     <div class="py-3 fs-2 bg-success text-white">40</div>
                 </div>
             </div>
-            
+
         </div>
         <div class="container-canvas">
-    <canvas id="myChart" width=200" height="150"></canvas>
-    </div>
-    <!---Tarjetas
+            <canvas id="myChart" width="200" height="150"></canvas>
+        </div>
+        <!---Tarjetas
         <div class="container">
         <div class="row">
             <div class="col-md-4">
@@ -171,9 +149,15 @@ $m_actual="SELECT * FROM m_actual";
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 
 
-<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"
+    integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+</script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
+    integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+</script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
+    integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+</script>
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.bundle.js"></script>
@@ -182,8 +166,6 @@ $m_actual="SELECT * FROM m_actual";
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.9.1/chart.min.js"></script>
 <script>
-
-
 // JavaScript code
 showTime();
 
@@ -222,58 +204,57 @@ function CargarGrafica() {
             cantidad_a.push(data[i][2]);
             cantidad_b.push(data[i][3]);
         }
-var ctx = document.getElementById("myChart").getContext("2d");
-var myBarChart = new Chart(ctx, {
-  type: 'bar',
-  options: {
-  scales: {
-    xAxes: [{
-      id: "bar-x-axis2",
-      stacked: true,
-      categoryPercentage: 0.5,
-      barPercentage: 0.5,
-    },  {
-      display: false,
-      id: "bar-x-axis1",
-      type: 'category',
-      categoryPercentage: 0.4,
-      barPercentage: 1,
-      gridLines: {
-        offsetGridLines: true
-      },
-      stacked: true
-    }],
-    yAxes: [{
-      max: 100,
-      min: 0,
-      stacked: true
-    }]
+        var ctx = document.getElementById("myChart").getContext("2d");
+        var myBarChart = new Chart(ctx, {
+            type: 'bar',
+            options: {
+                scales: {
+                    xAxes: [{
+                        id: "bar-x-axis2",
+                        stacked: true,
+                        categoryPercentage: 0.5,
+                        barPercentage: 0.5,
+                    }, {
+                        display: false,
+                        id: "bar-x-axis1",
+                        type: 'category',
+                        categoryPercentage: 0.4,
+                        barPercentage: 1,
+                        gridLines: {
+                            offsetGridLines: true
+                        },
+                        stacked: true
+                    }],
+                    yAxes: [{
+                        max: 100,
+                        min: 0,
+                        stacked: true
+                    }]
 
-  }
-},
-  data:{
-  labels: titulo,
-  datasets: [ {
-    label: "Target",
-    backgroundColor: 'red',
-    data: cantidad_a,
-    xAxisID: "bar-x-axis2",
-    stack: "background"
-  }, {
-    label: "Progreeso",
-    backgroundColor: 'rgba(0,0,0,0.5)',
-    data: cantidad_b,
-    xAxisID: "bar-x-axis2",
-    fill: false
-  }]
-}
+                }
+            },
+            data: {
+                labels: titulo,
+                datasets: [{
+                    label: "Target",
+                    backgroundColor: 'red',
+                    data: cantidad_a,
+                    xAxisID: "bar-x-axis2",
+                    stack: "background"
+                }, {
+                    label: "Progreeso",
+                    backgroundColor: 'rgba(0,0,0,0.5)',
+                    data: cantidad_b,
+                    xAxisID: "bar-x-axis2",
+                    fill: false
+                }]
+            }
 
 
-});
+        });
     })
 
 }
-
 </script>
 
 </html>
