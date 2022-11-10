eb9fe1cd87c0a26783e3e42a9983e94c0cc9bb2e
Cesar Alanis <cesar.alanis@na.denso.com>
2022-11-10 09:47:02 -0600

    Actualizacion mostrar modelo actual

--- config/conexion.php
+++ config/conexion.php
@@ -1,30 +1,5 @@
-<?php
 
-class Database
-{
-    private $hostname = "localhost";
-    private $database = "control-lotes";
-    private $username = "root";
-    private $password = "";
-    private $charset = "utf8";
-    
-    function conectar()
-    {
-        try {
-            $conexion = "mysql:host=" . $this->hostname . ";dbname=" . $this->database . ";charset=" .
-             $this->charset;
-            $options = [
-                        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                        PDO::ATTR_EMULATE_PREPARES => false,
-                    ];
-
-                $pdo = new PDO($conexion, $this->username, $this->password, $options);
-                return $pdo;
-
-                    } catch (PDOException $e) {
-                        echo 'Error conexion: ' . $e->getMessage();
-                            exit;
-                }
-    }
-}
+<?php 
+$conexion = mysqli_connect('localhost', 'root', '', 'grafico' );
+mysqli_set_charset($conexion,"utf8");
 ?>
