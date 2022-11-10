9a4a5f66f12fb1eda66f0be6f71f75f4eb20983a
Cesar Alanis <cesar.alanis@na.denso.com>
2022-11-10 10:04:54 -0600

    Organizacion codigo

--- SAT.php
+++ SAT.php
@@ -13,6 +13,7 @@ $sec="10";
 include("config/conexion.php");
 $modelos="SELECT modelo FROM modelos ORDER BY id DESC LIMIT 1";
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -62,19 +63,16 @@ $modelos="SELECT modelo FROM modelos ORDER BY id DESC LIMIT 1";
     <main class="m-5 p-5">
 
         <div class="border border-dark col-md-3 text-center">
-
             <div class="py-3">
                 <div class="py-3">
-
                     <h1 class="">Modelo Actual</h1>
                     <?php
-    $resultado=mysqli_query($conexion,$modelos);
-
-    while($row=mysqli_fetch_assoc($resultado)){?>
-                    <h1 class="col"><?php echo $row["modelo"];?></h1><?php }?>
-
+                        $resultado=mysqli_query($conexion,$modelos);
+                        while($row=mysqli_fetch_assoc($resultado)){
+                    ?>
+                            <h1 class="col"><?php echo $row["modelo"];?></h1>
+                    <?php }?>
                 </div>
-
             </div>
         </div>
 
@@ -88,76 +86,28 @@ $modelos="SELECT modelo FROM modelos ORDER BY id DESC LIMIT 1";
                     <div class="py-3 fs-2 bg-success text-white">40</div>
                 </div>
             </div>
-
         </div>
+
         <div class="container-canvas">
             <canvas id="myChart" width="200" height="150"></canvas>
         </div>
-        <!---Tarjetas
-        <div class="container">
-        <div class="row">
-            <div class="col-md-4">
-                <div class="bg-success text-white text-center m-1">
-                    <div class="card-header">Modelo</div>
-                    <div class="card-body">
-                        <h5 class="h1 card-title">
-                            <spain id="IdVendidos">35</spain>
-                        </h5>
-                        <p class="card-text">Baja en las ventas vs mes anterior</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="bg-warning text-white text-center m-1">
-                    <div class="card-header">Modelo 761</div>
-                    <div class="card-body">
-                        <h5 class="h1 card-title">
-                            <spain id="Idalmacen">35</spain>
-                        </h5>
-                        <p class="card-text">inventario vs mes anterior</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="bg-info text-white text-center m-1">
-                    <div class="card-header">Modelo</div>
-                    <div class="card-body">
-                        <h5 class="h1 card-title">
-                            <spain id="Idngreso">35</spain>
-                        </h5>
-                        <p class="card-text">disminucion de ingresos vs mes anterior</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="row my-3 ">
-            <div class="col-md-12 text-center ">
-                <h2>Reporte de ventas</h2>
-            </div>
-           
-        </div>
-        <div class="row my-3 ">
-            <div class="col-md-12 text-center "></div>
-            <canvas id="idcontTabla"></canvas>
-        </div>
-    </div>
-   --->
-
     </main>
 </body>
 
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 
-
 <script src="https://code.jquery.com/jquery-3.2.1.min.js"
     integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
 </script>
+
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
     integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
 </script>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
     integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
 </script>
+
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.bundle.js"></script>
@@ -166,6 +116,7 @@ $modelos="SELECT modelo FROM modelos ORDER BY id DESC LIMIT 1";
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.9.1/chart.min.js"></script>
 <script>
+
 // JavaScript code
 showTime();
 
